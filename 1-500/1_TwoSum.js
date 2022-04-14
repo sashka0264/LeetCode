4a5652031551339e672ad5bcc8aca9f837015957
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-04-14 22:56:23 +0300

    Сделал 1 задачу (Runtime - 70 ms, Memory - 42.9 MB).

---
+++ 1-500/1_TwoSum.js
@@ -0,0 +1,21 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+// target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+    let data = {};
+    for (let i = 0; i < nums.length; i++) {
+        const number = nums[i];
+        if (data[target - number] !== undefined) {
+            return [data[target - number], i];
+        } else {
+            data[number] = i;
+        }
+    }
+    return [];
+};
