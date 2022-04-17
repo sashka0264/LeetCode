e0f51ba747705d82542a1a7b4a44c108d3d20872
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-04-18 01:08:39 +0300

    Сделал 1480 задачу (Runtime - 72 ms, Memory - 42.4 MB).

--- Easy/1480_Easy_RunningSumOf1dArray.js
+++ Easy/1480_Easy_RunningSumOf1dArray.js
@@ -1,6 +1,6 @@
 /*
-Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). Return the running
-sum of nums.
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). Return the running sum
+of nums.
 
 Link: https://leetcode.com/problems/running-sum-of-1d-array
 
