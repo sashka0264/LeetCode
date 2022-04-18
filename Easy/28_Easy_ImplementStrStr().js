8ced2b5adb43b95263ef55e1d8b1d28882f7f38c
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-04-18 15:28:32 +0300

    Сделал 28 задачу (Runtime - 63 ms, Memory - 42.3 MB).

---
+++ Easy/28_Easy_ImplementStrStr().js
@@ -0,0 +1,20 @@
+/*
+Implement strStr(). Given two strings needle and haystack, return the index of the first occurrence of needle in
+haystack, or -1 if needle is not part of haystack. Clarification: What should we return when needle is an empty string?
+This is a great question to ask during an interview. For the purpose of this problem, we will return 0 when needle is an
+empty string. This is consistent to C's strstr() and Java's indexOf().
+
+Link: https://leetcode.com/problems/implement-strstr
+
+Solution:
+*/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = (haystack, needle) => {
+    const start = haystack.split(needle)[0].length
+    return haystack.length === start ? -1 : start;
+};
