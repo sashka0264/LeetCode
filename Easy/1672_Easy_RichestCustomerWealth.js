e96ab4e87b9f4ec9b7dd305ae1a128edd95bce34
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-04-20 00:27:36 +0300

    Сделал 1672 задачу

---
+++ Easy/1672_Easy_RichestCustomerWealth.js
@@ -0,0 +1,21 @@
+/*
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the ith customer has in the
+jth bank. Return the wealth that the richest customer has. A customer's wealth is the amount of money they have in all
+their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+Link: https://leetcode.com/problems/richest-customer-wealth
+
+Solution:
+*/
+
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+const maximumWealth = accounts => {
+    let m = 0;
+    for (let i = 0; i < accounts.length; i++) {
+        m = Math.max(m, accounts[i].reduce((a, b) => a + b));
+    }
+    return m;
+};
