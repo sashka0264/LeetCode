aebee10545c7ecb3f7e24f4fe7be97654747eb4d
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-04-19 12:06:28 +0300

    Сделал 3 задачу

---
+++ Easy/3_LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,23 @@
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+Link: https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+Solution:
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = s => {
+    const map = new Map();
+    let r = 0;
+
+    for (let i = 0, j = 0; i < s.length; i += 1) {
+        if (map.has(s[i])) j = Math.max(map.get(s[i]), j);
+        r = Math.max(r, i + 1 - j);
+        map.set(s[i], i + 1);
+    }
+    return r;
+};
