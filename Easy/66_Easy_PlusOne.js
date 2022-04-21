a1bc271850d26be033751ab8acaac89d99b484c3
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-04-22 00:51:43 +0300

    Сделал 66 задачу

---
+++ Easy/66_Easy_PlusOne.js
@@ -0,0 +1,25 @@
+/*
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the
+integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer
+does not contain any leading 0's. Increment the large integer by one and return the resulting array of digits.
+
+Link: https://leetcode.com/problems/plus-one
+
+Solution:
+*/
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+const plusOne = digits => {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        digits[i]++;
+        if (digits[i] < 10) {
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits.unshift(1);
+    return digits;
+};
