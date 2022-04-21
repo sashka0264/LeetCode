9bceb34094e34e3ea1dcbda799c23926ef2d190b
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-04-21 15:54:47 +0300

    Сделал 704 задачу

---
+++ Easy/704_Easy_BinarySearch.js
@@ -0,0 +1,26 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search
+target in nums. If target exists, then return its index. Otherwise, return -1. You must write an algorithm with O(log n)
+runtime complexity.
+
+Link: https://leetcode.com/problems/binary-search
+
+Solution:
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = (nums, target) => {
+    let left = 0, right = nums.length - 1;
+
+    while (left < right) {
+        const center = left + Math.ceil((right-left)/2);
+        if (target < nums[center]) {
+            right = center - 1;
+        } else left = center;
+    }
+    return nums[left] == target ? left : -1;
+};
