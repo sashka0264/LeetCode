2ae3d2e4e4f86cdc44b4af5e90d332d8d6ea0d5e
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-04-30 23:30:47 +0300

    Сделал 1290 задачу

---
+++ Easy/1290_Easy_ConvertBinaryNumberInALinkedListToInteger.js
@@ -0,0 +1,22 @@
+/*
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or
+1. The linked list holds the binary representation of a number. Return the decimal value of the number in the linked
+list.
+
+Link: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer
+
+Solution:
+*/
+
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+const getDecimalValue = head => {
+    let s = '';
+    while (head) {
+        s += head.val;
+        head = head.next;
+    }
+    return parseInt(s, 2);
+};
