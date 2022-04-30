531fdbefb6afff0bd2f11f7d6ba082f7dd7be633
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-05-01 01:27:45 +0300

    Сделал 1748 задачу

---
+++ Easy/1748_Easy_SumOfUniqueElements_24|93.js
@@ -0,0 +1,24 @@
+/*
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the
+array. Return the sum of all the unique elements of nums.
+
+Link: https://leetcode.com/problems/sum-of-unique-elements
+
+Solution:
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const sumOfUnique = nums => {
+    const map = new Map();
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum = sum + (map.has(nums[i]) ? -map.get(nums[i]) : nums[i]);
+        map.set(nums[i], map.has(nums[i]) ? 0 : nums[i]);
+    }
+
+    return sum;
+};
