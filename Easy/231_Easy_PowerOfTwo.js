cfe1aa5c193e7735d56c3f16992d7cd5b959a64c
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-05-03 23:08:55 +0300

    Сделал 231 задачу

---
+++ Easy/231_Easy_PowerOfTwo.js
@@ -0,0 +1,14 @@
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false. An integer n is a power of two, if
+there exists an integer x such that n == 2x.
+
+Link: https://leetcode.com/problems/power-of-two
+
+Solution:
+*/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfTwo = n => Math.log2(n) % 1 == 0;
