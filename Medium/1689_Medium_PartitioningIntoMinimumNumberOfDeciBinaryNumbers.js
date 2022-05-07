1a9d9988ee0523336c702407be2315e7e0479a7b
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-05-07 23:14:54 +0300

    Сделал 1689 задачу

---
+++ Medium/1689_Medium_PartitioningIntoMinimumNumberOfDeciBinaryNumbers.js
@@ -0,0 +1,21 @@
+/*
+A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example,
+101 and 1100 are deci-binary, while 112 and 3001 are not. Given a string n that represents a positive decimal integer,
+return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+
+Link: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers
+
+Solution:
+*/
+
+/**
+ * @param {string} n
+ * @return {number}
+ */
+const minPartitions = n => {
+    let max = 0;
+    for (let i = 0; i < n.length; i++) {
+        max = Math.max(n[i], max);
+    }
+    return max;
+};
