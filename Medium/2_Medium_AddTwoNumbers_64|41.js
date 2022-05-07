22917df4dfbf086e759b208bcf6d217a549f9645
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-05-07 15:46:04 +0300

    Решил 2 задачу

---
+++ Medium/2_Medium_AddTwoNumbers_64|41.js
@@ -0,0 +1,32 @@
+/*
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse 
+order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list. You may 
+assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Link: https://leetcode.com/problems/add-two-numbers
+
+Solution:
+*/
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ const addTwoNumbers = (l1, l2) => {
+    const head = new ListNode();
+    let cursor = head, carry = 0;
+
+    while (l1 || l2 || carry) {
+        cursor.next = new ListNode();
+        cursor = cursor.next;
+        let val = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        carry = val >= 10 ? 1 : 0;
+        cursor.val = val % 10;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+
+    return head.next;
+}
