d839addaf0de27c946047b1e6bfffadd50fbb0e1
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-05-09 16:52:31 +0300

    Сделал 1684 задачу

---
+++ Easy/1684_Easy_CountTheNumberOfConsistentStrings_32|28.js
@@ -0,0 +1,17 @@
+/*
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent
+if all characters in the string appear in the string allowed. Return the number of consistent strings in the array
+words.
+
+Link: https://leetcode.com/problems/count-the-number-of-consistent-strings
+
+Solution:
+*/
+
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+const countConsistentStrings = (allowed, words) => words
+    .filter(word => [...word].every(letter => allowed.includes(letter))).length;
