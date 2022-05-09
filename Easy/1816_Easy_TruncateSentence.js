c6eae432fd4efa0a69485409bb1339fe6cdb7f88
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-05-09 19:11:28 +0300

    Сделал 1816 задачу

---
+++ Easy/1816_Easy_TruncateSentence.js
@@ -0,0 +1,17 @@
+/*
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words
+consists of only uppercase and lowercase English letters (no punctuation). For example, "Hello World", "HELLO", and
+"hello world hello world" are all sentences. You are given a sentence s and an integer k. You want to truncate s such
+that it contains only the first k words. Return s after truncating it.
+
+Link: https://leetcode.com/problems/truncate-sentence
+
+Solution:
+*/
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+const truncateSentence = (s, k) => s.split(' ').splice(0, k).join(' ');
