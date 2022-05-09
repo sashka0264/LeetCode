6e6f94cf82abcd48adee1a6d72144e02c1d5379f
Колесников Александр Леонидович <ALeoKolesnikov@sberbank.ru>
2022-05-09 19:25:57 +0300

    Сделал 804 задачу

---
+++ Easy/804_Easy_UniqueMorseCodeWords.js
@@ -0,0 +1,39 @@
+/*
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as
+follows: 'a' maps to ".-", 'b' maps to "-...", 'c' maps to "-.-.", and so on. For convenience, the full table for the
+26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...",
+"-","..-","...-",".--","-..-","-.--","--.."]
+
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter. For
+example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such
+a concatenation the transformation of a word. Return the number of different transformations among all words we have.
+
+Link: https://leetcode.com/problems/unique-morse-code-words
+
+Solution:
+*/
+
+const s = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",
+    ".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+const uniqueMorseRepresentations = words => {
+    const map = [];
+    let strM = '';
+
+    for (let i = 0; i < words.length; i += 1) {
+        for (let y = 0; y < words[i].length; y += 1) {
+            const code = s[words[i][y].charCodeAt(0) - 97];
+            strM += code;
+        }
+        if (!map.includes(strM)) map.push(strM);
+        strM = '';
+    }
+
+    return map.length;
+};
