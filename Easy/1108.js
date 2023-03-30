65ae32529f770940cc340aa1dc478ecc1818e36f
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:29:34 +0300

    Update and rename 1108_Easy_DefangingAnIPAddress.js to 1108.js

--- Easy/1108.js
+++ Easy/1108.js
@@ -2,7 +2,7 @@
 Given a valid (IPv4) IP address, return a defanged version of that IP address. A defanged IP address replaces every
 period "." with "[.]".
 
-Link: https://leetcode.com/problems/defanging-an-ip-address
+https://leetcode.com/problems/defanging-an-ip-address
 
 Solution:
 */
