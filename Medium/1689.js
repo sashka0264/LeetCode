65cad84f8acc7b1dc9115b027fb4e0554426b8f0
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:05:04 +0300

    Update and rename 1689_Medium_PartitioningIntoMinimumNumberOfDeciBinaryNumbers.js to 1689.js

--- Medium/1689.js
+++ Medium/1689.js
@@ -3,9 +3,7 @@ A decimal number is called deci-binary if each of its digits is either 0 or 1 wi
 101 and 1100 are deci-binary, while 112 and 3001 are not. Given a string n that represents a positive decimal integer,
 return the minimum number of positive deci-binary numbers needed so that they sum up to n.
 
-Link: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers
-
-Solution:
+https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers
 */
 
 /**
