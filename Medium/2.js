221500ddfdab2d7a562f360c0f8dfcac8a27d509
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:06:57 +0300

    Update and rename 2_Medium_AddTwoNumbers_64|41.js to 2.js

--- Medium/2.js
+++ Medium/2.js
@@ -4,9 +4,7 @@ You are given two non-empty linked lists representing two non-negative integers.
 order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list. You may 
 assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-Link: https://leetcode.com/problems/add-two-numbers
-
-Solution:
+https://leetcode.com/problems/add-two-numbers
 */
 
 /**
