4963012095f686a0b1bea740c9ab2f977e9725bb
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:43:03 +0300

    Update and rename 66_Easy_PlusOne.js to 66.js

--- Easy/66.js
+++ Easy/66.js
@@ -3,9 +3,7 @@ You are given a large integer represented as an integer array digits, where each
 integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer
 does not contain any leading 0's. Increment the large integer by one and return the resulting array of digits.
 
-Link: https://leetcode.com/problems/plus-one
-
-Solution:
+https://leetcode.com/problems/plus-one
 */
 
 /**
