28eecebd0a266f3baf32c0b5a525ea542fcdc86d
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:40:43 +0300

    Update and rename 69_Easy_Sqrt(x).js to 69.js

--- Easy/69.js
+++ Easy/69.js
@@ -3,9 +3,7 @@ Given a non-negative integer x, compute and return the square root of x. Since t
 decimal digits are truncated, and only the integer part of the result is returned. Note: You are not allowed to use any
 built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
 
-Link: https://leetcode.com/problems/sqrtx
-
-Solution:
+https://leetcode.com/problems/sqrtx
 */
 
 /**
