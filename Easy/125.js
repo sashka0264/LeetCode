0abf3efef06a105625b55a16621fa602638c3ad1
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:29:54 +0300

    Update and rename 125_Easy_ValidPalindrome.js to 125.js

--- Easy/125.js
+++ Easy/125.js
@@ -3,7 +3,7 @@ A phrase is a palindrome if, after converting all uppercase letters into lowerca
 non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and
 numbers. Given a string s, return true if it is a palindrome, or false otherwise.
 
-Link: https://leetcode.com/problems/valid-palindrome
+https://leetcode.com/problems/valid-palindrome
 
 Solution:
 */
