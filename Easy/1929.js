b8fd6cadecbbc8747e8ba4fd3b2ab0ad8a9d954f
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:56:31 +0300

    Update and rename 1929_Easy_ConcatenationOfArray_77|25.js to 1929.js

--- Easy/1929.js
+++ Easy/1929.js
@@ -3,9 +3,7 @@ Given an integer array nums of length n, you want to create an array ans of leng
 ans[i + n] == nums[i] for 0 <= i < n (0-indexed). Specifically, ans is the concatenation of two nums arrays. Return the
 array ans.
 
-Link: https://leetcode.com/problems/concatenation-of-array
-
-Solution:
+https://leetcode.com/problems/concatenation-of-array
 */
 
 /**
