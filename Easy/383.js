9526e7164395eaf92ecb4d236b76589e0632405b
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:48:29 +0300

    Update and rename 383_Easy_RansomNote.js to 383.js

--- Easy/383.js
+++ Easy/383.js
@@ -2,9 +2,7 @@
 Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false
 otherwise. Each letter in magazine can only be used once in ransomNote.
 
-Link: https://leetcode.com/problems/ransom-note
-
-Solution:
+https://leetcode.com/problems/ransom-note
 */
 
 /**
