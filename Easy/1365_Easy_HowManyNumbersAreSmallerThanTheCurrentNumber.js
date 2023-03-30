a65f0c693b9fe56e253660b10b5f08c49fc06188
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:35:28 +0300

    Update 1365_Easy_HowManyNumbersAreSmallerThanTheCurrentNumber.js

--- Easy/1365_Easy_HowManyNumbersAreSmallerThanTheCurrentNumber.js
+++ Easy/1365_Easy_HowManyNumbersAreSmallerThanTheCurrentNumber.js
@@ -2,9 +2,7 @@
 Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each
 nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i]. Return the answer in an array.
 
-Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number
-
-Solution:
+https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number
 */
 
 /**
