e01527292d72a603400d1ad3f566d89614b5c72a
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:53:13 +0300

    Update and rename 1773_Easy_CountItemsMatchingARule.js to 1773.js

--- Easy/1773.js
+++ Easy/1773.js
@@ -1,12 +1,10 @@
 /*
-ou are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the
 ith item. You are also given a rule represented by two strings, ruleKey and ruleValue. The ith item is said to match the
 rule if one of the following is true: ruleKey == "type" and ruleValue == typei, ruleKey == "color" and ruleValue
 == colori, ruleKey == "name" and ruleValue == namei. Return the number of items that match the given rule.
 
-Link: https://leetcode.com/problems/count-items-matching-a-rule
-
-Solution:
+https://leetcode.com/problems/count-items-matching-a-rule
 */
 
 /**
