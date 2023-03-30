ae05ca0055f7f0af1932f3de3abf183819e098a4
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:57:19 +0300

    Update and rename 1967_Easy_NumberOfStringsThatAppearAsSubstringsInWord.js to 1967.js

--- Easy/1967.js
+++ Easy/1967.js
@@ -2,9 +2,7 @@
 Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring
 in word. A substring is a contiguous sequence of characters within a string.
 
-Link: https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word
-
-Solution:
+https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word
 */
 
 /**
