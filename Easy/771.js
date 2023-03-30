c610a98640fa87ade3ddf25b3086483776543aa1
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:38:39 +0300

    Update and rename 771_Easy_JewelsAndStones.js to 771.js

--- Easy/771.js
+++ Easy/771.js
@@ -3,9 +3,7 @@ You're given strings jewels representing the types of stones that are jewels, an
 have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also
 jewels. Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
-Link: https://leetcode.com/problems/jewels-and-stones
-
-Solution:
+https://leetcode.com/problems/jewels-and-stones
 */
 
 /**
