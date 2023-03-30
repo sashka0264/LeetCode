9b8613d07ce0b4033cd783ef1f13262cb2f3e6e5
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:00:58 +0300

    Update and rename 2108_Easy_FindFirstPalindromicStringInTheArray_28|28.js to 2108.js

--- Easy/2108.js
+++ Easy/2108.js
@@ -2,9 +2,7 @@
 Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an
 empty string "". A string is palindromic if it reads the same forward and backward.
 
-Link: https://leetcode.com/problems/find-first-palindromic-string-in-the-array
-
-Solution:
+https://leetcode.com/problems/find-first-palindromic-string-in-the-array
 */
 
 /**
