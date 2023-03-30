25401149a95f41828446f499b928d3c0cdb8a732
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:02:18 +0300

    Update and rename 278_Easy_FirstBadVersion.js to 278.js

--- Easy/278.js
+++ Easy/278.js
@@ -5,9 +5,7 @@ Suppose you have n versions [1, 2, ..., n] and you want to find out the first ba
 ones to be bad. You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a
 function to find the first bad version. You should minimize the number of calls to the API.
 
-Link: https://leetcode.com/problems/first-bad-version
-
-Solution:
+https://leetcode.com/problems/first-bad-version
 */
 
 /**
