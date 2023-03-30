45151d4e2380208df286d3768800d441c6d8ea29
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:48:03 +0300

    Update and rename 1684_Easy_CountTheNumberOfConsistentStrings_32|28.js to 1684.js

--- Easy/1684.js
+++ Easy/1684.js
@@ -3,9 +3,7 @@ You are given a string allowed consisting of distinct characters and an array of
 if all characters in the string appear in the string allowed. Return the number of consistent strings in the array
 words.
 
-Link: https://leetcode.com/problems/count-the-number-of-consistent-strings
-
-Solution:
+https://leetcode.com/problems/count-the-number-of-consistent-strings
 */
 
 /**
