c0e5cbf204695be9c1f04f69da244aaa0bc4e471
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:44:23 +0300

    Update and rename 1662_Easy_CheckIfTwoStringArraysAreEquivalent.js to 1662.js

--- Easy/1662.js
+++ Easy/1662.js
@@ -2,9 +2,7 @@
 Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise. A
 string is represented by an array if the array elements concatenated in order forms the string.
 
-Link: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent
-
-Solution:
+https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent
 */
 
 /**
