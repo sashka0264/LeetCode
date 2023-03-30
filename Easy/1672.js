3be306f5ff9a569de9aefc0ab14addf35f96af6e
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:46:11 +0300

    Update and rename 1672_Easy_RichestCustomerWealth.js to 1672.js

--- Easy/1672.js
+++ Easy/1672.js
@@ -3,9 +3,7 @@ You are given an m x n integer grid accounts where accounts[i][j] is the amount 
 jth bank. Return the wealth that the richest customer has. A customer's wealth is the amount of money they have in all
 their bank accounts. The richest customer is the customer that has the maximum wealth.
 
-Link: https://leetcode.com/problems/richest-customer-wealth
-
-Solution:
+https://leetcode.com/problems/richest-customer-wealth
 */
 
 /**
