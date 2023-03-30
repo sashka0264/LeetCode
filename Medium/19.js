a2da32283e9998814c53a774a1056fe8511b937e
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:06:20 +0300

    Update and rename 19_Medium_RemoveNthNodeFromEndOfList.js to 19.js

--- Medium/19.js
+++ Medium/19.js
@@ -1,9 +1,7 @@
 /*
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
-Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list
-
-Solution:
+https://leetcode.com/problems/remove-nth-node-from-end-of-list
 */
 
 /**
