196360e31b086b0213e8a7ae29d511f4e82e5338
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:41:05 +0300

    Update and rename 206_Easy_ReverseLinkedList_62|56.js to 206.js

--- Easy/206.js
+++ Easy/206.js
@@ -1,9 +1,7 @@
 /*
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 
-Link: https://leetcode.com/problems/reverse-linked-list
-
-Solution:
+https://leetcode.com/problems/reverse-linked-list
 */
 
 /**
