c3c37967dc359b6fc5bdf859741acefb81d89b91
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:01:38 +0300

    Update and rename 2185_Easy_CountingWordsWithAGivenPrefix.js to 2185.js

--- Easy/2185.js
+++ Easy/2185.js
@@ -2,9 +2,7 @@
 You are given an array of strings words and a string pref. Return the number of strings in words that contain pref as a
 prefix. A prefix of a string s is any leading contiguous substring of s.
 
-Link: https://leetcode.com/problems/counting-words-with-a-given-prefix
-
-Solution:
+https://leetcode.com/problems/counting-words-with-a-given-prefix
 */
 
 /**
