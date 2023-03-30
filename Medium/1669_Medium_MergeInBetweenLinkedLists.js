56bcd702906dbd6f7fbfa5970eb43c98e0d28ec0
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:04:12 +0300

    Update 1669_Medium_MergeInBetweenLinkedLists.js

--- Medium/1669_Medium_MergeInBetweenLinkedLists.js
+++ Medium/1669_Medium_MergeInBetweenLinkedLists.js
@@ -2,9 +2,7 @@
 You are given two linked lists: list1 and list2 of sizes n and m respectively. Remove list1's nodes from the ath node to
 the bth node, and put list2 in their place.
 
-Link: https://leetcode.com/problems/merge-in-between-linked-lists
-
-Solution:
+https://leetcode.com/problems/merge-in-between-linked-lists
 */
 
 /**
