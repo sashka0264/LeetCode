a73301f74f52aee083912af8a7d77b477b788a23
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:33:36 +0300

    Update and rename 977_Easy_SquaresOfASortedArray.js to 977.js

--- Easy/977.js
+++ Easy/977.js
@@ -2,9 +2,7 @@
 Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in
 non-decreasing order.
 
-Link: https://leetcode.com/problems/squares-of-a-sorted-array
-
-Solution:
+https://leetcode.com/problems/squares-of-a-sorted-array
 */
 
 /**
