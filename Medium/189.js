e0802b6c0dff8eda4a3b2b9b9d7396380f4b9172
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:05:59 +0300

    Update and rename 189_Medium_RotateArray.js to 189.js

--- Medium/189.js
+++ Medium/189.js
@@ -1,9 +1,7 @@
 /*
 Given an array, rotate the array to the right by k steps, where k is non-negative.
 
-Link: https://leetcode.com/problems/rotate-array
-
-Solution:
+https://leetcode.com/problems/rotate-array
 */
 
 /**
