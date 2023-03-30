96f79764884c8f90a71b48bd8bba6e56e01bcc4e
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:35:58 +0300

    Update 136_Easy_SingleNumber.js

--- Easy/136_Easy_SingleNumber.js
+++ Easy/136_Easy_SingleNumber.js
@@ -2,9 +2,7 @@
 Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 
-Link: https://leetcode.com/problems/single-number
-
-Solution:
+https://leetcode.com/problems/single-number
 */
 
 /**
