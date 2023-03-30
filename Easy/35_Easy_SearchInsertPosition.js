bf8aeea676ea2baf411cdc775d1112b717bfa3ba
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:51:20 +0300

    Update 35_Easy_SearchInsertPosition.js

--- Easy/35_Easy_SearchInsertPosition.js
+++ Easy/35_Easy_SearchInsertPosition.js
@@ -2,9 +2,7 @@
 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return
 the index where it would be if it were inserted in order. You must write an algorithm with O(log n) runtime complexity.
 
-Link: https://leetcode.com/problems/search-insert-position
-
-Solution:
+https://leetcode.com/problems/search-insert-position
 */
 
 /**
