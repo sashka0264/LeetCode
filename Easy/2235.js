8ddd3d3ce0ae7237fff06bd330752d2dc3b21fa2
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 13:03:49 +0300

    Update and rename 2235_Easy_AddTwoIntegers.js to 2235.js

--- Easy/2235.js
+++ Easy/2235.js
@@ -1,9 +1,7 @@
 /*
 Given two integers num1 and num2, return the sum of the two integers.
 
-Link: https://leetcode.com/problems/add-two-integers
-
-Solution:
+https://leetcode.com/problems/add-two-integers
 */
 
 /**
