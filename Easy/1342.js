0d65d543ad8267faeaea2e8e7c2567ffc40bc682
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:34:46 +0300

    Update and rename 1342_Easy_NumberOfStepsToReduceANumberToZero.js to 1342.js

--- Easy/1342.js
+++ Easy/1342.js
@@ -2,9 +2,7 @@
 Given an integer num, return the number of steps to reduce it to zero. In one step, if the current number is even, you
 have to divide it by 2, otherwise, you have to subtract 1 from it.
 
-Link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero
-
-Solution:
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero
 */
 
 /**
