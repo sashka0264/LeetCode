20bd9e9a87fd246b4a4aeebda9dba2e432cf3f74
Alexandr Kolesnikov <47452731+sashka0264@users.noreply.github.com>
2023-03-30 12:44:02 +0300

    Update and rename 557_Easy_ReverseWordsInAStringIII.js to 557.js

--- Easy/557.js
+++ Easy/557.js
@@ -2,9 +2,7 @@
 Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and
 initial word order.
 
-Link: https://leetcode.com/problems/reverse-words-in-a-string-iii
-
-Solution:
+https://leetcode.com/problems/reverse-words-in-a-string-iii
 */
 
 /**
